24c09c3378914854a89c06891a0088bfc8d6be67
hlccs <148472010+hlccs@users.noreply.github.com>
2023-10-25 12:05:24 -0400

    JQuery

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
-    $.ajax({
-        url: 'navbar.html',
-        type: 'GET',
-        success: function(data) {
-            $('#nav-place').html(data);
-        },
-        error: function() {
-            console.error('Failed to load navbar.html');
-        }
-    });
+    $("$nav-place").load("navbar.html")    
 });
