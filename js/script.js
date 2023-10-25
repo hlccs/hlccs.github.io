6a679fc292cbcd69ed7f9baa9c32a4045ce1f5db
harshil <48635660+Red12345lol@users.noreply.github.com>
2023-10-25 11:47:03 -0400

    Troubleshoot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    $(function(){
-        $("#nav-place").load("../navbar.html"); 
+    $.ajax({
+        url: 'navbar.html',
+        type: 'GET',
+        success: function(data) {
+            $('#nav-place').html(data);
+        },
+        error: function() {
+            console.error('Failed to load navbar.html');
+        }
     });
-})
+});
